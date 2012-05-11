d61e4669c8e60e3eda5448d41f8ab0e5bac6ece6
David John Evans <david@djevans.info>
2012-05-11 12:04:47 +0100

    Link Language ISO to all standard autocomplete fields

--- js/iso-639-2.js
+++ js/iso-639-2.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
-  console.log('Loaded JS');
+    
   Drupal.jsAC.prototype.select = function (node) {
-    console.log('Hi there');
-  }
+    this.input.value = this.selected.autocompleteValue;
+    var code = this.selected.innerText.match(/\[(\w+)\]/)[1];
+    // now populate the linked field
+    $('input[name*="language_iso"]').val(code);
+  };
+
+  Drupal.jsAC.prototype.hidePopup = function (keycode) {
+    var ac = this;
+    // Select item if the right key or mousebutton was pressed
+    if (this.selected && ((keycode && keycode != 46 && keycode != 8 && keycode != 27) || !keycode)) {
+      ac.select(this); // this.input.value = this.selected.autocompleteValue; // don't duplicate code!
+    }
+    // Hide popup
+    var popup = this.popup;
+    if (popup) {
+      this.popup = null;
+      $(popup).fadeOut('fast', function() { $(popup).remove(); });
+    }
+    this.selected = false;
+  };
 });
