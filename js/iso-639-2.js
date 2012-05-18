5846fa8dd9ad06d653f4141d52b399e11be6271e
David John Evans <david@djevans.info>
2012-05-18 22:12:05 +0100

    Check for jsAC

--- js/iso-639-2.js
+++ js/iso-639-2.js
@@ -1,26 +1,30 @@
-$(document).ready(function() {
-    
-  Drupal.jsAC.prototype.select = function (node) {
-    this.input.value = this.selected.autocompleteValue;
-    if (this.input.name.match(/language_text/)) {
-      var code = this.selected.innerText.match(/\[(\w+)\]/)[1];
-      // now populate the linked field
-      $('input[name*="language_iso"]').val(code);
-    }
-  };
+if (Drupal.jsAC) {
 
-  Drupal.jsAC.prototype.hidePopup = function (keycode) {
-    var ac = this;
-    // Select item if the right key or mousebutton was pressed
-    if (this.selected && ((keycode && keycode != 46 && keycode != 8 && keycode != 27) || !keycode)) {
-      ac.select(this); // this.input.value = this.selected.autocompleteValue; // don't duplicate code!
-    }
-    // Hide popup
-    var popup = this.popup;
-    if (popup) {
-      this.popup = null;
-      $(popup).fadeOut('fast', function() { $(popup).remove(); });
-    }
-    this.selected = false;
-  };
-});
+  $(document).ready(function() {
+   
+    Drupal.jsAC.prototype.select = function (node) {
+      this.input.value = this.selected.autocompleteValue;
+      if (this.input.name.match(/language_text/)) {
+        var code = this.selected.innerText.match(/\[(\w+)\]/)[1];
+        // now populate the linked field
+        $('input[name*="language_iso"]').val(code);
+      }
+    };
+
+    Drupal.jsAC.prototype.hidePopup = function (keycode) {
+      var ac = this;
+      // Select item if the right key or mousebutton was pressed
+      if (this.selected && ((keycode && keycode != 46 && keycode != 8 && keycode != 27) || !keycode)) {
+        ac.select(this); // this.input.value = this.selected.autocompleteValue; // don't duplicate code!
+      }
+      // Hide popup
+      var popup = this.popup;
+      if (popup) {
+        this.popup = null;
+        $(popup).fadeOut('fast', function() { $(popup).remove(); });
+      }
+      this.selected = false;
+    };
+  });
+
+}
